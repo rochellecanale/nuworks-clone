c846a3b411fef4869b7f2372b40fd1b61dada0dc
Rochelle Canale <chelle@cbo.me>
2022-05-31 14:22:00 +0800

    minor css modification

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -3,18 +3,3 @@ $(document).scroll(function () {
 	$nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	$(".navbar .navbar-brand").toggleClass('logo-show', $(this).scrollTop() < $nav.height());
 });
-
-$("#open-nav").on("click", function () {
-	$("#menu").addClass("open");
-	$window = $(window);
-	if ($window.width() < 1024) {
-		$("body").css('overflow', 'hidden');
-	}
-})
-
-$("#close-nav").on("click", function () {
-	$("#menu").removeClass("open");
-	if ($window.width() < 1024) {
-		$("body").css('overflow', 'unset');
-	}
-})
