d9d8865bb62a0fe43da53cd0fbcf0f4fe10b5f0a
Rochelle Canale <chelle@cbo.me>
2022-06-03 07:47:50 +0800

    Added back2top

--- src/components/FloatingLogo/FloatingLogo.js
+++ src/components/FloatingLogo/FloatingLogo.js
@@ -7,11 +7,14 @@ function FloatingLogo(props) {
 		name: "loader-icon",
 		image: "./assets/images/rotating-logo.gif"
 	}
+
+	const handleClick = () => {
+		window.scrollTo(1000, 0);
+	}
+
 	return (
-		<div className="persistent-logo">
-			<a href={props.url ? props.url : defaultValues.url }>
-				<img src={props.image ? props.image : defaultValues.image } alt={props.name ? props.name : defaultValues.name} />
-			</a>
+		<div onClick={handleClick} className="persistent-logo">
+			<img src={props.image ? props.image : defaultValues.image } alt={props.name ? props.name : defaultValues.name} />
 		</div>
 	)
 }
